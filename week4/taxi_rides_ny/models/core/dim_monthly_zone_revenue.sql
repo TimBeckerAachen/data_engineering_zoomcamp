0953446922e5d6b63cf34057ab0c13298a3d5116
Tim Becker <timb1909@gmail.com>
2022-02-22 20:42:21 +0000

    try with manually uploaded data

--- week4/taxi_rides_ny/models/core/dim_monthly_zone_revenue.sql
+++ week4/taxi_rides_ny/models/core/dim_monthly_zone_revenue.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='table') }}
 
 with trips_data as (
-    select * from {{ ref('fact_trips') }}
+    select * from {{ ref('fact_trips_manual') }}
 )
     select 
     -- Reveneue grouping 
